6305f23a6a531f5c022c73cf307e24ea5a654265
Aron <aronkerk8@gmail.com>
2023-08-22 18:50:54 -0300

    AuthProvider com user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import Rotas from './routes';
+import AuthProvider from './components/AuthProvider';
 
 function App() {
   return (
-    <Rotas/>
+    <AuthProvider>
+      <Rotas/>
+    </AuthProvider>
   );
 }
 
